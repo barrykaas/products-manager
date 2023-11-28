7ba6e5cc4b180a3ba41f6deb0d5ed45e1290c78d
juulco <juliancolen@gmail.com>
2023-11-28 20:03:57 +0100

    usePersons hook

--- src/Persons/PersonsApiQueries.js
+++ src/Persons/PersonsApiQueries.js
@@ -1,8 +1,21 @@
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 import apiPath from "../Api/ApiPath";
 
 
+const personsQueryKey = 'persons';
+
 export const getPersonsFn = async () => {
     return axios.get(`${apiPath}/persons/`);
+};
+
+export const usePersons = () => {
+    const { isError, error, isLoading, data } = useQuery({ queryKey: [personsQueryKey], queryFn: getPersonsFn });
+    const actualData = data?.data || [];
+    const getPerson = (id) => {
+        const matches = actualData.filter((item) => id === item.id);
+        return matches[0] || null;
+    };
+    return {isError, error, isLoading, data: actualData, getPerson};
 };
