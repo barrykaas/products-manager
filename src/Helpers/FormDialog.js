02ed8a5c3ae809fc53a2d86fa4c3a71ee2a3e1f8
juulco <juliancolen@gmail.com>
2023-12-14 18:41:59 +0100

    fixed appbar

--- src/Helpers/FormDialog.js
+++ src/Helpers/FormDialog.js
@@ -20,29 +20,29 @@ function FormDialog({ open, onClose, title, secondaryButtons, hasToolbar = true,
       maxWidth='sm'
       TransitionComponent={TransitionRight}
     >
-      { hasToolbar ? 
-      <AppBar sx={{ position: 'relative' }}>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            onClick={onClose}
-            aria-label="close"
-          >
-            <CloseIcon />
-          </IconButton>
-          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-            {title}
-          </Typography>
-          {secondaryButtons}
-          {/* <Button autoFocus color="inherit" onClick={handleClose}>
+      {hasToolbar ?
+        <AppBar position="sticky">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={onClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              {title}
+            </Typography>
+            {secondaryButtons}
+            {/* <Button autoFocus color="inherit" onClick={handleClose}>
                             save
                         </Button> */}
 
 
-        </Toolbar>
-      </AppBar>
-:<></>}
+          </Toolbar>
+        </AppBar>
+        : <></>}
       {children}
     </Dialog>
   );
