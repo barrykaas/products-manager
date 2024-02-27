de8740293333c61204048195ee07c30e157282f9
juulco <juliancolen@gmail.com>
2024-02-28 00:52:28 +0100

    wrap in ControllerView

--- src/Settings/SettingsPage.js
+++ src/Settings/SettingsPage.js
@@ -2,22 +2,28 @@ import { useCookies } from "react-cookie";
 import { Stack, Typography } from "@mui/material";
 
 import { PersonsIdField } from "../Persons/PersonsField";
+import ControllerView from "../Helpers/ControllerView";
 
 
-export default function SettingsPage() {
+export default function SettingsPage({ onMenu }) {
     const [cookies, setCookie] = useCookies(["user"]);
 
     return (
-        <Stack component="form">
-            <Typography variant="h6">Wie ben je?</Typography>
-            <PersonsIdField
-                label="Kies gebruiker"
-                value={cookies?.user && Number(cookies.user)}
-                setValue={(userId) => setCookie(
-                    "user", userId, { expires: new Date("2030-01-01T00:00:00Z") }
-                )}
-            />
-        </Stack>
+        <ControllerView
+            title="Instellingen"
+            onMenu={onMenu}
+        >
+            <Stack component="form">
+                <Typography variant="h6">Wie ben je?</Typography>
+                <PersonsIdField
+                    label="Kies gebruiker"
+                    value={cookies?.user && Number(cookies.user)}
+                    setValue={(userId) => setCookie(
+                        "user", userId, { expires: new Date("2030-01-01T00:00:00Z") }
+                    )}
+                />
+            </Stack>
+        </ControllerView>
     );
 }
 
