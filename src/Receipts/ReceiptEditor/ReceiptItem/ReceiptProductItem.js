532f172fc35cf873f77b65864120a0b2f8637713
juulco <juliancolen@gmail.com>
2024-03-05 17:10:00 +0100

    small refactor

--- src/Receipts/ReceiptEditor/ReceiptItem/ReceiptProductItem.js
+++ src/Receipts/ReceiptEditor/ReceiptItem/ReceiptProductItem.js
@@ -1,17 +1,67 @@
-
-import { Box, Skeleton, Typography, Stack, Grid, IconButton } from "@mui/material";
+import { useState } from "react";
+import { Box, Skeleton, Typography, Stack, IconButton, ListItem } from "@mui/material";
 import { Delete } from "@mui/icons-material";
 
-import { useListItemDeleter } from "../../../Lists/ListsApiQueries";
+import { useListItemDeleter, useListItemMutator } from "../../../Lists/ListsApiQueries";
 import { useBrands } from "../../../Brands/BrandsApiQueries";
 import ProductTooltip from "../../../Products/ProductTooltip";
 import QuantityController from "./QuantityController";
+import FormDialog from "../../../Helpers/FormDialog";
+import ProductController from "../../../Products/ProductController";
 
 
 export default function ReceiptProductItem({ item }) {
+    const [productPickerOpen, setProductPickerOpen] = useState(false);
     const deleteListItem = useListItemDeleter();
+    const mutateListItem = useListItemMutator();
     const product = item.product;
 
+    const onDelete = () => {
+        deleteListItem(item.id);
+    };
+
+    const handleReplacementProduct = (product) => {
+        mutateListItem({
+            id: item.id,
+            product_id: product.id
+        }, { onSuccess: () => setProductPickerOpen(false) });
+    };
+
+    return (
+        <ListItem disableGutters>
+            <Stack spacing={1} width={1}>
+
+                <Stack
+                    sx={{ px: 2 }}
+                    direction="row" alignItems="center" justifyContent="flex-start"
+                    spacing={1}
+                >
+                    <ProductInfo product={product} />
+                    <Box flexGrow={1} />
+
+                    <IconButton onClick={onDelete} color="error">
+                        <Delete />
+                    </IconButton>
+                </Stack>
+
+                <QuantityController listItem={item} />
+            </Stack>
+
+            <FormDialog
+                open={productPickerOpen}
+                hasToolbar={false}
+            >
+                <ProductController
+                    onClose={() => setProductPickerOpen(false)}
+                    handleSelectedProduct={handleReplacementProduct}
+                />
+            </FormDialog>
+        </ListItem>
+    );
+}
+
+
+function ProductInfo({ product }) {
     const brandsQuery = useBrands();
     const brandName = brandsQuery.getBrand(product.brand)?.name;
 
@@ -22,39 +72,18 @@ export default function ReceiptProductItem({ item }) {
         return <Skeleton />
     }
 
-    const onDelete = () => {
-        deleteListItem(item.id);
-    };
-
     return (
-        <Box>
-            <Stack sx={{ py: 1 }} spacing={1}>
-                <Stack
-                    sx={{ px: 2 }}
-                    direction="row" alignItems="center" justifyContent="space-between"
-                >
-                    <Grid container alignItems="baseline" spacing={1}>
-                        <Grid item>
-                            <ProductTooltip product={product}>
-                                <Typography display="inline" variant="h6" component="div" >
-                                    {product.name}
-                                </Typography>
-                            </ProductTooltip>
-                        </Grid>
-                        <Grid item>
-                            <Typography display="inline" color="text.secondary" variant="body2">
-                                {brandName}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-
-                    <IconButton onClick={onDelete} color="error">
-                        <Delete />
-                    </IconButton>
-                </Stack>
-
-                <QuantityController listItem={item} />
-            </Stack>
-        </Box>
+        <Stack alignItems="flex-start">
+            {brandName &&
+                <Typography variant="subtitle2" color="text.secondary">
+                    {brandName}
+                </Typography>
+            }
+            <ProductTooltip product={product}>
+                <Typography variant="h6">
+                    {product.name}
+                </Typography>
+            </ProductTooltip>
+        </Stack>
     );
 }
