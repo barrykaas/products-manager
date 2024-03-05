fa227c0c5a7693b4071234ee00cb95081e8ec994
juulco <juliancolen@gmail.com>
2024-03-05 23:46:39 +0100

    support listitems with no event

--- src/Receipts/ReceiptEditor/ReceiptEventBlock.js
+++ src/Receipts/ReceiptEditor/ReceiptEventBlock.js
@@ -10,92 +10,27 @@ import { EventFormDialog } from "../../Events/EventForm";
 import { useQuery } from "@tanstack/react-query";
 
 
-function AddProductButton({ variant = "outlined", ...args }) {
-    return (
-        <Button variant="outlined" {...args}>
-            + Product
-        </Button>
-    );
-}
-
-function AddAmountButton({ variant = "outlined", ...args }) {
-    return (
-        <Button variant="outlined" color="warning" {...args}>
-            + Los bedrag
-        </Button>
-    );
-}
-
-
-function Header({ itemId, title, date, personIds, onAddProduct, onAddAmount, onEdit }) {
-    const formattedDate = isoToRelativeDate(date);
-
-    return (
-        <Grid container spacing={2} sx={{ p: 2 }} alignItems="center">
-            <Grid item>
-                <Tooltip arrow title={
-                    <Typography variant="caption" fontFamily="monospace">
-                        ID: {itemId}
-                    </Typography>
-                }>
-                    <Typography variant="h6" component="div">
-                        {title}
-                    </Typography>
-                </Tooltip>
-            </Grid>
-
-            {onEdit ?
-                <Grid item>
-                    <IconButton size="small" onClick={onEdit}>
-                        <Edit />
-                    </IconButton>
-                </Grid>
-                : null}
-
-            {date
-                ? (
-                    <Grid item>
-                        <Chip label={formattedDate} variant="outlined" />
-                    </Grid>
-                ) : null
-            }
-
-            <Grid item>
-                <PersonAvatarGroup personIds={personIds} />
-            </Grid>
-
-            <Grid item>
-                <ButtonGroup size="small" variant="outlined" aria-label="outlined button group">
-                    <AddProductButton onClick={onAddProduct} />
-                    <AddAmountButton onClick={onAddAmount} />
-                </ButtonGroup>
-            </Grid>
-        </Grid>
-    );
-}
-
-
 export default function ReceiptEventBlock({ eventId, eventItems, onAddProduct, onAddDiscount }) {
-    const eventQuery = useQuery({ queryKey: ['events', eventId] });
+    const eventQuery = useQuery({ queryKey: ['events', eventId], enabled: !!eventId });
     const [editingEvent, setEditingEvent] = useState(false);
 
     const event = eventQuery.data;
-    let eventName = event?.name ?? `Event ${eventId}`;
-    if (eventQuery.isLoading) {
-        eventName = "Event laden...";
+
+    let eventName
+    if (eventId) {
+        eventName = event?.name ?? `Event ${eventId}`;
+        if (eventQuery.isLoading) {
+            eventName = "Event laden...";
+        }
     }
 
     const eventTotal = eventItems.reduce((s, i) => s + i.amount, 0);
-    const participantIds = event?.event_participants ?? [];
 
     return (
         <Paper>
             <Stack>
                 <Header
-                    itemId={eventId}
-                    title={eventName}
-                    date={event?.event_date}
-                    personIds={participantIds}
+                    event={event}
                     onAddProduct={onAddProduct}
                     onAddAmount={onAddDiscount}
                     onEdit={() => setEditingEvent(true)}
@@ -129,7 +64,9 @@ export default function ReceiptEventBlock({ eventId, eventItems, onAddProduct, o
                 </Stack>
                 <Stack direction="row" spacing={1} sx={{ px: 2, py: 1 }}>
                     <Typography component="div">
-                        Totaal van <em>{eventName}</em>
+                        Totaal van {eventId ?
+                            <em>{eventName}</em>
+                            : "items zonder event"}
                     </Typography>
                     <div style={{ flexGrow: 1 }}></div>
                     <Typography variant="h6" component="div" sx={{ whiteSpace: "nowrap" }}>
@@ -139,6 +76,77 @@ export default function ReceiptEventBlock({ eventId, eventItems, onAddProduct, o
 
             </Stack>
         </Paper>
+    );
+}
+
+function AddProductButton({ variant = "outlined", ...args }) {
+    return (
+        <Button variant="outlined" {...args}>
+            + Product
+        </Button>
+    );
+}
+
+function AddAmountButton({ variant = "outlined", ...args }) {
+    return (
+        <Button variant="outlined" color="warning" {...args}>
+            + Los bedrag
+        </Button>
+    );
+}
+
+function Header({ event, onAddProduct, onAddAmount, onEdit }) {
+    const formattedDate = isoToRelativeDate(event?.event_date);
+    const participantIds = event?.event_participants;
+    const eventName = event?.name;
+
+    return (
+        <Grid container spacing={2} sx={{ p: 2 }} alignItems="center">
+            <Grid item>
+                {event ?
+                    <>
+                        <Tooltip arrow title={
+                            <Typography variant="caption" fontFamily="monospace">
+                                ID: {event?.id}
+                            </Typography>
+                        }>
+                            <Typography variant="h6" component="div">
+                                {eventName}
+                            </Typography>
+                        </Tooltip>
+                    </>
+                    : <Typography variant="h6" component="div" fontStyle="italic">
+                        Geen event
+                    </Typography>
+                }
+            </Grid>
+
+            {onEdit && event?.id &&
+                <Grid item>
+                    <IconButton size="small" onClick={onEdit}>
+                        <Edit />
+                    </IconButton>
+                </Grid>}
+
+            {formattedDate &&
+                <Grid item>
+                    <Chip label={formattedDate} variant="outlined" />
+                </Grid>
+            }
+
+            {participantIds &&
+                <Grid item>
+                    <PersonAvatarGroup personIds={participantIds} />
+                </Grid>
+            }
+
+            <Grid item>
+                <ButtonGroup size="small" variant="outlined" aria-label="outlined button group">
+                    <AddProductButton onClick={onAddProduct} />
+                    <AddAmountButton onClick={onAddAmount} />
+                </ButtonGroup>
+            </Grid>
+        </Grid>
     );
 }
 
